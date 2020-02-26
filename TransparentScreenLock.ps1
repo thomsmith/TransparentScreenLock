efae6d7b5aa49bcc573f5eb8a2b2dfa32c8dd350
Tom Smith <thomdsmith@gmail.com>
2020-02-26 17:55:20 +0000

    Add files via upload

---
+++ TransparentScreenLock.ps1
@@ -0,0 +1,19 @@
+﻿
+$xCmdString = {rundll32.exe user32.dll,LockWorkStation}
+
+#$process = Get-Process | Where-Object { $_.ProcessName -eq "bubbles"} -ErrorAction SilentlyContinue
+#$processname = $process.name
+
+
+Invoke-Item "C:\Windows\System32\Bubbles.scr"
+
+Start-Sleep -Seconds 2
+
+Do {
+  $process = Get-Process | Where-Object { $_.Path -eq "C:\Windows\System32\Bubbles.scr"}
+ }
+ until($process -eq $null)
+
+Invoke-Command $xCmdString
+
+exit
